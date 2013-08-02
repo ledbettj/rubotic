303a3cbdfad09428a4ebe979ec382d1b46bb8f05
John Ledbetter <john.ledbetter@gmail.com>
2013-08-02 18:20:59 -0400

    add another reddit command.

--- lib/plugins/reddit.rb
+++ lib/plugins/reddit.rb
@@ -4,11 +4,23 @@ class RedditPlugin < Rubotic::Plugin
   describe "Returns results from reddit"
 
   command '!aww' do
-    describe  "returns a random cute picture from the front page of r/aww."
+    describe "returns a random cute picture from the front page of r/aww."
 
     run do |event|
-      item = @client.browse('aww').sample
+      item = @client.browse('aww', limit: 50).sample
       respond_to(event, item.url)
+    end
+  end
+
+  command '!r' do
+    describe  "returns a random item from the provided subreddit."
+    arguments 1..1
+    usage     '<subreddit>'
+
+    run do |event, subreddit|
+      item = @client.browse(subreddit, limit: 50).sample
+      respond_to(event,
+          "[#{sprintf('+%d', item.score)}] #{truncate(item.title)} (#{item.url})")
     end
   end
 
@@ -16,4 +28,9 @@ class RedditPlugin < Rubotic::Plugin
     @bot = bot
     @client = Reddit::Api.new
   end
+
+  def truncate(text, length=38)
+    text[0..length].gsub(/\s\w+$/, '...')
+  end
+
 end
