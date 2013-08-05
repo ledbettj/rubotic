0fe9ac64f1b245a090bea0e4e97f207b30c3c466
John Ledbetter <john.ledbetter@gmail.com>
2013-08-05 19:24:09 -0400

    make help a multi-level affair

--- lib/plugins/help.rb
+++ lib/plugins/help.rb
@@ -2,13 +2,41 @@ class HelpPlugin < Rubotic::Plugin
   describe "Show help for commands"
 
   command '!help' do
-    describe  "display all commands or help on a single command"
+    describe  "display all plugins or help on a single plugin or command."
     arguments 0..1
-    usage     "[command]"
+    usage     "[plugin or command]"
 
     run do |event, cmd = nil|
-      cmd ? help_command(event, cmd) : list_commands(event)
+      if cmd
+        help_plugin_or_cmd(event, cmd)
+      else
+        list_plugins(event)
+      end
     end
+  end
+
+  def list_plugins(event)
+    [
+      respond_to(event,
+        "use !help <pluginname> to see commands for a plugin",
+        private: true
+      )
+    ] +
+      bot.plugins.map do |p|
+      respond_to(event,
+        "#{p.class.name}: #{p.class.description}",
+        private: true
+      )
+    end
+  end
+
+  def help_plugin_or_cmd(event, cmd)
+    plug = Rubotic::Plugin.registered.find do |p|
+      p.name.downcase == cmd.downcase ||
+      p.name.downcase.gsub('plugin', '') == cmd.downcase
+    end
+
+    plug ? list_commands(event, plug) : help_command(event, cmd)
   end
 
   def help_command(event, cmd)
@@ -24,19 +52,14 @@ class HelpPlugin < Rubotic::Plugin
     end
   end
 
-  def list_commands(event)
-    bot.plugins.flat_map do |plugin|
-      [
-        respond_to(event,
-          "#{plugin.class.name}:",
-          private: true
-        )
-      ] + plugin.class.commands.map do |trigger, cmd|
-        respond_to(event,
-          "  #{trigger}: #{cmd.describe}",
-          private: true
-        )
-      end
+  def list_commands(event, plugin)
+    [
+      respond_to(event, "#{plugin.name} commands:", private: true)
+    ] + plugin.commands.map do |trigger, cmd|
+      respond_to(event,
+        "  #{trigger}: #{cmd.describe}",
+        private: true
+      )
     end
   end
 end
