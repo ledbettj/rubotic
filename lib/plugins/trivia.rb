6f1dc8b51542a8b8f1000d90ef1794a16a07b8cf
John Ledbetter <john@callrail.com>
2014-07-25 15:05:24 -0400

    derps.

--- lib/plugins/trivia.rb
+++ lib/plugins/trivia.rb
@@ -8,11 +8,11 @@ class TriviaPlugin < Rubotic::Plugin
     arguments 0..0
 
     run do |event|
-      if can_get_new?
+      if !current_question
         new_question
         respond_to(event, "#{current_question[:category]}: #{current_question[:question]}", private: false)
       else
-        respond_to(event, "Can't ask a new question right now. Answer the first one!")
+        respond_to(event, "Can't ask a new question right now. Answer the first one or !giveup", private: false)
       end
     end
   end
@@ -20,12 +20,23 @@ class TriviaPlugin < Rubotic::Plugin
   command '!score' do
     describe 'see trivia scores for a user'
     usage '<user>'
-    arguments 1
+    arguments 1..1
 
     run do |event, who|
       row = @bot.db[:trivia_scores].where(nick: who).first
       pts = row ? row[:score] : 0
       respond_to(event, "#{who}: #{pts} points.")
+    end
+  end
+
+  command '!giveup' do
+    describe 'shameful'
+    arguments 0..0
+
+    run do |event|
+      if current_question && timed_out?
+        respond_to(event, "The answer was: #{current_question[:answer]}.  Use !trivia for a new question.", private: false)
+      end
     end
   end
 
@@ -39,7 +50,7 @@ class TriviaPlugin < Rubotic::Plugin
         if current_question[:answer].downcase == answer
           clear_question
           point_for(event.from.nick)
-          respond_to(event, "#{event.from.nick} is right! Huzzah! Use !trivia for a new question.", public: false)
+          respond_to(event, "#{event.from.nick} is right! Huzzah! Use !trivia for a new question.", private: false)
         end
       else
         respond_to(event, "No active question! Use !trivia to ask one.")
@@ -66,13 +77,17 @@ class TriviaPlugin < Rubotic::Plugin
     @asked_at = nil
   end
 
+  def timed_out?
+    asked_at && (asked_at + 30) < Time.now
+  end
+
   def new_question
     @current_question = config[:questions].sample
     @asked_at = Time.now
   end
 
   def can_get_new?
-    current_question.nil? || (asked_at + 30) >= Time.now
+    current_question.nil? || timed_out?
   end
 
   def initialize(bot)
